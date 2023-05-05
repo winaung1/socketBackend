84c2be6b1be47f8d6a0178fb5359aa15ca7dc6e0
Win Aung <91712429+winaung1@users.noreply.github.com>
2023-05-04 21:30:23 -0600

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3001;
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://winsocket.onrender.com",
         methods: ["GET", "POST"],
     }
 });
