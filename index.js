ca9396ab14560c84e0691eb93c11ef03c0520b5a
Win Aung <91712429+winaung1@users.noreply.github.com>
2023-05-04 20:36:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http")
+const {Server} = require('socket.io');
+const cors = require('cors')
+app.use(cors())
+const server = http.createServer(app);
+const port = 3001;
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
+
+
+//listen to events
+io.on("connection", (socket) => {
+    console.log(`User connected ${socket.id}`)
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+    })
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data)
+    })
+})
+
+
+server.listen(port, () => {
+    console.log(`server is running on ${port}`)
+})
